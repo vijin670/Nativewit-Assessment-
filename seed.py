9a1fce2bf0d4bae6ab98f39db468183239f3fce7
VIJIN AJAI V <vijin670@gmail.com>
2026-07-02 17:48:19 +0530

    Update seed.py

--- seed.py
+++ seed.py
@@ -4,12 +4,10 @@ from models import User, Coupon
 
 with app.app_context():
 
-    # Create sample users
     if not User.query.first():
         db.session.add(User(name="Vijin"))
         db.session.add(User(name="Rahul"))
 
-    # Create sample coupon
     if not Coupon.query.filter_by(code="FREE100").first():
         db.session.add(Coupon(
             code="FREE100",
